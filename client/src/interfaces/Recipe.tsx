9eae23b2a1e5c7dc7005cf88471aab4d316c004d
Wyatt7709 <wwoydziak.7704@gmail.com>
2024-12-03 20:43:54 -0600

    Added recipe and ingrediant interface, optinal comments implemented but hashed out tell further notice

---
+++ client/src/interfaces/Recipe.tsx
@@ -0,0 +1,22 @@
+export interface Recipe { // Recipe in full detail
+    id: string; // Unique identifier
+    name: string;
+    author: string;
+    servingSize: string;
+    ingredients: string[]; // Array of ingredient strings
+    instructions: string[]; // Step-by-step instructions
+    tags: string[]; // Array of tags (e.g., 'vegetarian', 'gluten-free')
+    imageUrl: string; // URL of image file
+    // comments?: string[]; // Optional comments
+  }
+  
+
+export interface RecipeSummary {   // Recipe in summary form
+    id: string; // Unique identifier
+    name: string;
+    author: string;
+    servingSize: string;
+    tags: string[]; // Array of tags (e.g., 'vegetarian', 'gluten-free')
+    imageUrl: string; // URL of image file
+}
+  
