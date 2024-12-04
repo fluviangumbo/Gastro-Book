32b3de7983a02b4730a7b61acec7d04eb7de59c4
JosephEyler1 <josepheyler1@gmail.com>
2024-12-03 19:26:19 -0600

    changed format of file structure

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,38 +1,36 @@
 import { gql } from '@apollo/client';
 
-const MUTATIONS = {
-  ADD_USER: gql`
-    mutation AddUser($input: UserInput!) {
-      addUser(input: $input) {
-        token
-        user {
-          _id
-          username
-          email
-          recipe
-        }
-      }
-    }
-  `,
-
-  LOGIN: gql`
-    mutation Login($email: String!, $password: String!) {
-      login(email: $email, password: $password) {
-        token
-        user {
-          _id
-          username
-          email
-          recipe
-        }
-      }
-    }
-  `,
-
-  ADD_RECIPE: gql`
-    mutation AddRecipe($input: [RecipeInput]!) {
-      addRecipe(input: $input) {
+export const ADD_USER = gql`
+  mutation AddUser($input: UserInput!) {
+    addUser(input: $input) {
+      token
+      user {
         _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_RECIPE = gql`
+  mutation AddRecipe($input: [RecipeInput]!) {
+    addRecipe(input: $input) {
+      _id
+      recipes {
         recipeName
         recipeAuthor {
           _id
@@ -49,12 +47,15 @@ const MUTATIONS = {
         }
       }
     }
-  `,
+  }
+`;
 
-  ADD_COMMENT: gql`
-    mutation AddComment($recipeId: ID!, $commentText: String!) {
-      addComment(recipeId: $recipeId, commentText: $commentText) {
-        _id
+export const ADD_COMMENT = gql`
+  mutation AddComment($recipeId: ID!, $commentText: String!) {
+    addComment(recipeId: $recipeId, commentText: $commentText) {
+      _id
+      recipes {
+        recipeName
         recipeComments {
           _idRecipe
           commentText
@@ -62,29 +63,30 @@ const MUTATIONS = {
         }
       }
     }
-  `,
+  }
+`;
 
-  REMOVE_RECIPE: gql`
-    mutation RemoveRecipe($RecipeId: ID!) {
-      removeRecipe(RecipeId: $RecipeId) {
-        _id
+export const REMOVE_RECIPE = gql`
+  mutation RemoveRecipe($RecipeId: ID!) {
+    removeRecipe(RecipeId: $RecipeId) {
+      _id
+      recipes {
         recipeName
       }
     }
-  `,
+  }
+`;
 
-  REMOVE_COMMENT: gql`
-    mutation RemoveComment($recipeId: ID!, $commentId: ID!) {
-      removeComment(recipeId: $recipeId, commentId: $commentId) {
-        _id
+export const REMOVE_COMMENT = gql`
+  mutation RemoveComment($recipeId: ID!, $commentId: ID!) {
+    removeComment(recipeId: $recipeId, commentId: $commentId) {
+      _id
+      recipes {
         recipeComments {
           _idRecipe
           commentText
-          createdAt
         }
       }
     }
-  `,
-};
-
-export default MUTATIONS;
+  }
+`;
--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,80 +1,73 @@
 import { gql } from '@apollo/client';
 
-const QUERIES = {
-  GET_USERS: gql`
-    query GetUsers {
-      users {
+export const GET_USERS = gql`
+  query GetUsers {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query GetUser($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_RECIPES = gql`
+  query GetRecipes {
+    recipe {
+      _id
+      recipeName
+      recipeAuthor {
         _id
         username
-        email
-        recipe
+      }
+      servingSize
+      ingredients
+      instructions
+      recipeComments {
+        _idRecipe
+        commentText
+        createdAt
       }
     }
-  `,
+  }
+`;
 
-  GET_USER: gql`
-    query GetUser($username: String!) {
-      user(username: $username) {
+export const GET_RECIPE_BY_NAME = gql`
+  query GetRecipeByName($recipeName: String!) {
+    recipeName(recipeName: $recipeName) {
+      _id
+      recipeName
+      recipeAuthor {
         _id
         username
-        email
-        recipe
+      }
+      servingSize
+      ingredients
+      instructions
+      recipeComments {
+        _idRecipe
+        commentText
+        createdAt
       }
     }
-  `,
+  }
+`;
 
-  GET_RECIPES: gql`
-    query GetRecipes {
-      recipe {
-        _id
-        recipeName
-        recipeAuthor {
-          _id
-          username
-        }
-        servingSize
-        ingredients
-        instructions
-        recipeComments {
-          _idRecipe
-          commentText
-          createdAt
-        }
-      }
+export const GET_ME = gql`
+  query GetMe {
+    me {
+      _id
+      username
+      email
     }
-  `,
-
-  GET_RECIPE_BY_NAME: gql`
-    query GetRecipeByName($recipeName: String!) {
-      recipeName(recipeName: $recipeName) {
-        _id
-        recipeName
-        recipeAuthor {
-          _id
-          username
-        }
-        servingSize
-        ingredients
-        instructions
-        recipeComments {
-          _idRecipe
-          commentText
-          createdAt
-        }
-      }
-    }
-  `,
-
-  GET_ME: gql`
-    query GetMe {
-      me {
-        _id
-        username
-        email
-        recipe
-      }
-    }
-  `,
-};
-
-export default QUERIES;
+  }
+`;
