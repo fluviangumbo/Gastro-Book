48e9365b7acfed7d177a84333c715bfe75fa1446
dTop <jroney002@gmail.com>
2024-12-04 18:51:59 -0600

    recipe schema updates to model, typedefs to follow

--- server/src/models/Recipe.ts
+++ server/src/models/Recipe.ts
@@ -1,17 +1,13 @@
 // Week18-Activity26-Solved - we didint think we needed it
 import { Schema, model, Document } from 'mongoose';
-import bcrypt from 'bcrypt';
 
 // Define an interface for the User document
 interface IRecipe extends Document {
   recipeName: string;
-  recipeAuthor: string;
-  servingSize: String;
-  ingredients: string;
-  instructions: string;
-  
-  recipe: Schema.Types.ObjectId[];
-  
+  recipeAuthor: Schema.Types.ObjectId;
+  servingSize: string;
+  ingredients: string[];
+  instructions: string[];
 }
 
 // Define the schema for the Recipe document
@@ -24,42 +20,29 @@ const recipeSchema = new Schema<IRecipe>(
       trim: true,
     },
     recipeAuthor: {
-      type: String,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
       required: true,
       unique: false,
-      trim: true,
     },
     servingSize: {
       type: String,
       required: true,
       unique: false,
     },
-    ingredients: {
+    ingredients: [{
       type: String,
       required: true,
       unique: false,
       trim: true,
-    },
-    instructions: {
+    }],
+    instructions: [{
       type: String,
       required: true,
       unique: false,
-    },
-    recipe: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Recipe',
-    }],
-  },
-  {
-    timestamps: true,
-    toJSON: { getters: true },
-    toObject: { getters: true },
+    }]
   }
 );
-
-recipeSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
-  return bcrypt.compare(password, this.password);
-};
 
 const Recipe = model<IRecipe>('Recipe', recipeSchema);
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -45,4 +45,4 @@ const startApolloServer = async () => {
   });
 };
 
-startApolloServer();
+await startApolloServer();
